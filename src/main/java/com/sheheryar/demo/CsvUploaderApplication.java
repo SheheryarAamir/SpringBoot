41e5b139b900d9a0057321d201c656a9e9213e6c
Sheheryar <sheheryar_Aamir@hotmail.com>
2017-05-28 15:06:34 +0400

    First Commint

---
+++ src/main/java/com/sheheryar/demo/CsvUploaderApplication.java
@@ -0,0 +1,12 @@
+package com.sheheryar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvUploaderApplication.class, args);
+	}
+}
